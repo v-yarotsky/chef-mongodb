76ca1293a07be9f62af18f5291f30fa51c9482d4
Vladimir Yarotsky <v.yarotsky@itransition.com>
2014-01-09 22:13:15 +0300

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'mongodb'
+maintainer       'Vladimir Yarotsky'
+maintainer_email 'vladimir.yarotsky@gmail.com'
+license          'MIT'
+description      'Installs/Configures mongodb'
+version          '0.0.1'
+
+depends "apt"
